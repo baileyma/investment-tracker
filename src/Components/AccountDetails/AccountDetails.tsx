85e8daf83b383c85b4dbe0baf01c1192abf146e4
Matt Bailey <baileyma@tcd.ie>
2025-01-05 18:46:50 +0000

    complete first draft of account details page

--- src/Components/AccountDetails/AccountDetails.tsx
+++ src/Components/AccountDetails/AccountDetails.tsx
@@ -48,6 +48,7 @@ const AccountDetails = () => {
   }, []);
 
   const getBalances = async () => {
+    console.log('this gets called');
     try {
       const response = await axios.get(
         `http://localhost:8080/accounts/balances/${id}/${year}`
@@ -94,17 +95,57 @@ const AccountDetails = () => {
       month: event.target.month.value,
       day: event.target.day.value,
     };
-    const response = await axios.post('http://localhost:8080/', paymentObj);
-    return response;
+    const ID = await axios.post(
+      `http://localhost:8080/updates/payment/${id}/${year}`,
+      paymentObj
+    );
+    const response = await getPayments();
+    setPayments(response);
+    const responseXIRR = await getXIRR();
+    setXIRR(responseXIRR);
+    return ID;
   };
 
-  if (!payments || !accounts || !balances || !XIRR) {
+  const postBalances = async (event) => {
+    event.preventDefault();
+    const balancesObj = {
+      start: event.target.start.value,
+      end: event.target.end.value,
+      month: event.target.month.value,
+      day: event.target.day.value,
+    };
+    console.log(balancesObj);
+    const ID = await axios.post(
+      `http://localhost:8080/updates/balances/${id}/${year}`,
+      balancesObj
+    );
+    const response = await getBalances();
+    console.log(response);
+    setBalances(response);
+    return ID;
+  };
+
+  const deletePayment = async (paymentID) => {
+    const ID = await axios.delete(
+      `http://localhost:8080/updates/delete/${id}/${year}/${paymentID}`
+    );
+    const response = await getPayments();
+    setPayments(response);
+    const responseXIRR = await getXIRR();
+    setXIRR(responseXIRR);
+
+    return ID;
+  };
+
+  if (!accounts) {
     return <h2>Loading....</h2>;
   }
 
   return (
     <>
-      <p className="Details__last-updated">Last updated: xx/xx/xx</p>
+      <p className="Details__last-updated">
+        Latest balance: {balances[0]?.day}/{balances[0]?.month}/{year}
+      </p>
 
       <div className="Details__title-wrapper">
         <h2 className="Details__title">
@@ -115,48 +156,131 @@ const AccountDetails = () => {
         </h2>
       </div>
 
-      <h2 className="Details__xirr">XIRR : {(XIRR * 100).toFixed(2)}%</h2>
+      <h2 className="Details__xirr">
+        Return (XIRR): {(XIRR * 100).toFixed(2)}%
+      </h2>
 
       <div className="Details__balance-wrapper">
         <div>
           <h2>
-            Balance 1st Jan: <br /> £{nfObject.format(balances[0].start)}
+            Balance 1st Jan: <br /> £{nfObject.format(balances[0]?.start)}
           </h2>
+          <button>Edit</button>
           <h2>
-            Balance 31st Dec: <br /> £{nfObject.format(balances[0].end)}
+            Balance {balances[0]?.day}/{balances[0]?.month}: <br /> £
+            {nfObject.format(balances[0]?.end)}
           </h2>
+          <button>Edit</button>
         </div>
-        <div>
-          <h2>Add balances</h2>
-          <form onSubmit={(e) => postPayment(e)}>
-            <div>
-              <label name="amount">Jan 1st</label>
-              <input name="amount" id="amount" type="number" />
-            </div>
 
-            <div className="Details__form">
-              <label name="month">Dec 31st</label>
-              <input name="month" id="month" type="number" />
-            </div>
+        <div className="Details__balance-inputs">
+          <form className="Details__form" onSubmit={(e) => postBalances(e)}>
+            <label name="start">Jan 1st</label>
+            <input
+              name="start"
+              id="start"
+              type="number"
+              defaultValue={balances[0]?.start || 0}
+            />
+
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <label name="end">Balance</label>
+
+            <input
+              name="end"
+              id="end"
+              type="number"
+              defaultValue={balances[0]?.end || 0}
+            />
+            <br />
+
+            <select name="month" id="month">
+              <option disabled selected>
+                ---Month---
+              </option>
+              <option value={0}>1</option>
+              <option value={1}>2</option>
+              <option value={2}>3</option>
+              <option value={3}>4</option>
+              <option value={4}>5</option>
+              <option value={5}>6</option>
+              <option value={6}>7</option>
+              <option value={7}>8</option>
+              <option value={8}>9</option>
+              <option value={9}>10</option>
+              <option value={10}>11</option>
+              <option value={11}>12</option>
+            </select>
+
+            <label name="day">Day</label>
+            <select name="day" id="day">
+              <option disabled selected>
+                ---Day---
+              </option>
+              <option value={0}>1</option>
+              <option value={1}>2</option>
+              <option value={2}>3</option>
+              <option value={3}>4</option>
+              <option value={4}>5</option>
+              <option value={5}>6</option>
+              <option value={6}>7</option>
+              <option value={7}>8</option>
+              <option value={8}>9</option>
+              <option value={9}>10</option>
+              <option value={10}>11</option>
+              <option value={11}>12</option>
+              <option value={12}>13</option>
+              <option value={13}>14</option>
+              <option value={14}>15</option>
+              <option value={15}>16</option>
+              <option value={16}>17</option>
+              <option value={17}>18</option>
+              <option value={18}>19</option>
+              <option value={19}>20</option>
+              <option value={20}>21</option>
+              <option value={21}>22</option>
+              <option value={22}>23</option>
+              <option value={23}>24</option>
+              <option value={24}>25</option>
+              <option value={25}>26</option>
+              <option value={26}>27</option>
+              <option value={27}>28</option>
+              <option value={28}>29</option>
+              <option value={29}>30</option>
+              <option value={30}>31</option>
+            </select>
             <button type="submit">Submit</button>
           </form>
         </div>
       </div>
       <div className="Details__payments-wrapper">
-        <div>
+        <div className="Details__payments-list">
           <h2>Payments</h2>
+          {!payments.length && <p>No payments entered</p>}
           {payments.map((payment) => {
             return (
-              <h3
-                className={
-                  payment.amount >= 0
-                    ? 'Details__deposited'
-                    : 'Details__withdrawn'
-                }
-              >
-                {nfObject.format(Math.abs(payment.amount))} ---
-                {new Date(payment.when).toLocaleDateString('en-GB')}
-              </h3>
+              <>
+                <div key={payment.id} className="Details__payment-item">
+                  <h3
+                    className={
+                      payment.amount >= 0
+                        ? 'Details__deposited'
+                        : 'Details__withdrawn'
+                    }
+                  >
+                    {nfObject.format(Math.abs(payment.amount))} ---
+                    {new Date(payment.when).toLocaleDateString('en-GB')}
+                  </h3>
+
+                  <button onClick={() => deletePayment(payment.id)}>
+                    Delete
+                  </button>
+                </div>
+              </>
             );
           })}
         </div>
@@ -170,12 +294,64 @@ const AccountDetails = () => {
 
             <div className="Details__form">
               <label name="month">Month</label>
-              <input name="month" id="month" type="number" />
+              <select name="month" id="month" type="number">
+                <option disabled selected>
+                  ---Month---
+                </option>
+                <option value={0}>1</option>
+                <option value={1}>2</option>
+                <option value={2}>3</option>
+                <option value={3}>4</option>
+                <option value={4}>5</option>
+                <option value={5}>6</option>
+                <option value={6}>7</option>
+                <option value={7}>8</option>
+                <option value={8}>9</option>
+                <option value={9}>10</option>
+                <option value={10}>11</option>
+                <option value={11}>12</option>
+              </select>
             </div>
 
             <div className="Details__form">
               <label name="day">Day</label>
-              <input name="day" id="day" type="number" />
+
+              <select name="day" id="day" type="number">
+                <option disabled selected>
+                  ---Day---
+                </option>
+                <option value={0}>1</option>
+                <option value={1}>2</option>
+                <option value={2}>3</option>
+                <option value={3}>4</option>
+                <option value={4}>5</option>
+                <option value={5}>6</option>
+                <option value={6}>7</option>
+                <option value={7}>8</option>
+                <option value={8}>9</option>
+                <option value={9}>10</option>
+                <option value={10}>11</option>
+                <option value={11}>12</option>
+                <option value={12}>13</option>
+                <option value={13}>14</option>
+                <option value={14}>15</option>
+                <option value={15}>16</option>
+                <option value={16}>17</option>
+                <option value={17}>18</option>
+                <option value={18}>19</option>
+                <option value={19}>20</option>
+                <option value={20}>21</option>
+                <option value={21}>22</option>
+                <option value={22}>23</option>
+                <option value={23}>24</option>
+                <option value={24}>25</option>
+                <option value={25}>26</option>
+                <option value={26}>27</option>
+                <option value={27}>28</option>
+                <option value={28}>29</option>
+                <option value={29}>30</option>
+                <option value={30}>31</option>
+              </select>
             </div>
 
             <button type="submit">Submit</button>
